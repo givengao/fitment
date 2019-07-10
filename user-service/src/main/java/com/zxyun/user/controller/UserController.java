3640d3f04f8c75fd841f5134041a56f365157f93
gaozonghui <123456>
2019-07-10 18:56:33 +0800

    1.用户服务

--- user-service/src/main/java/com/zxyun/user/controller/UserController.java
+++ user-service/src/main/java/com/zxyun/user/controller/UserController.java
@@ -4,6 +4,7 @@ import com.zxyun.user.annotation.Auth;
 import com.zxyun.user.util.BResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,5 +15,21 @@ public class UserController {
     @Auth
     public BResponse ping () {
         return BResponse.ok("hello world");
+    }
+
+    @PostMapping("/register")
+    public BResponse register () {
+        return BResponse.ok();
+    }
+
+    @PostMapping("/login")
+    public BResponse login () {
+        return BResponse.ok();
+    }
+
+    @PostMapping("/changePwd")
+    @Auth
+    public BResponse changePwd () {
+        return BResponse.ok();
     }
 }
